2d7e5ff5086e8a3e627d6510ddb1ced243fecbf3
华伟煌 <weihuang.hua@ucarinc.com>
2020-11-24 18:34:14 +0800

    @EnableUserClient相关

--- ch-1/springboot-demo/src/main/java/com/example/demo/SpringbootDemoApplication.java
+++ ch-1/springboot-demo/src/main/java/com/example/demo/SpringbootDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.user.EnableUserClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -7,7 +8,12 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.demo.filter.CustomFilter;
 
-//@EnableUserClient
+//@EnableUserClient注解与springboot-starter-demo下的spring.factories
+// 里的org.springframework.boot.autoconfigure.EnableAutoConfiguration=com.example.user.UserAutoConfigure
+// 必须要有一个，否则会报错
+// spring.factories 文件里指定了需要被装配的类
+// spring-configuration-metadata.json 是配置提示文件，非必需
+@EnableUserClient
 @SpringBootApplication
 public class SpringbootDemoApplication {
 

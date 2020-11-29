c1b46690cf62e4599df6435cfb6ee3469b855bcd
hwhcoder <hwhforever@163.com>
2020-11-29 16:43:20 +0800

    1

--- ch-2/eureka-server/src/main/java/com/example/eureka/EurekaServerApplication.java
+++ ch-2/eureka-server/src/main/java/com/example/eureka/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * http://localhost:8761/
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
--- ch-3/ribbon/src/main/java/com/example/ribbon/RibbonApplication.java
+++ ch-3/ribbon/src/main/java/com/example/ribbon/RibbonApplication.java
@@ -3,6 +3,10 @@ package com.example.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动EurekaServerApplication、UserApplication
+ * http://localhost:8082/get
+ */
 @SpringBootApplication
 public class RibbonApplication {
 

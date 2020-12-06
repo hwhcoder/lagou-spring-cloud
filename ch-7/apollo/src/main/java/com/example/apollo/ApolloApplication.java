7cc5c346f759bbc88b1e57e5228632697a38f90f
hwhcoder <hwhforever@163.com>
2020-12-06 16:59:17 +0800

    apollo 灰度发布

--- ch-7/apollo/src/main/java/com/example/apollo/ApolloApplication.java
+++ ch-7/apollo/src/main/java/com/example/apollo/ApolloApplication.java
@@ -3,6 +3,12 @@ package com.example.apollo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * Apollo 中，对于配置的修改，也可以选择灰度发布，推送到某个指定的节点，
+ * 如果这个节点使用一段时间没有发生问题，那么就可以推送给全部节点。
+ * 如果这个节点使用过程中出问题，那么直接回滚这个节点的配置即可，对于其他节点没有任何影响。
+ */
 @SpringBootApplication
 public class ApolloApplication {
 
